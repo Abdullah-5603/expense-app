b28be6a76d66bb8e4ce1b4856bf37f06f9bf21e7
Abdullah-5603 <abdullah72308@gmail.com>
2025-09-23 22:19:32 +0600

    query and server component updated

--- app/expenses/page.js
+++ app/expenses/page.js
@@ -1,12 +1,27 @@
-import ExpensesClient from "@/components/ExpenseClient";
+"use client";
+
 import { useAuth } from "@/context/AuthContext";
+import { useEffect, useState } from "react";
+import ExpensesClient from "@/components/ExpenseClient";
 import { getExpensesByMonth } from "@/lib/expenses";
 import { getCurrentMonthYear } from "@/utils/helper";
 
-export default async function ExpensesPage() {
-  const {user} = useAuth();
+export default function ExpensesPage() {
+  const { user } = useAuth();
+  const [initialData, setInitialData] = useState(null);
+
   const currentMonth = getCurrentMonthYear();
-  const initialData = await getExpensesByMonth(currentMonth, user.email);
+
+  useEffect(() => {
+    if (!user) return;
+    (async () => {
+      const data = await getExpensesByMonth(currentMonth, user.email);
+      setInitialData(data);
+    })();
+  }, [user, currentMonth]);
+
+  if (!user) return <p>Loading...</p>;
+  if (!initialData) return <h1>Fetching expenses...</h1>;
 
   return (
     <ExpensesClient
