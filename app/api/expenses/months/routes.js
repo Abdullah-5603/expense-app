e219049136d8436579bffe2b00bff67f8f8dc958
MD Abu Abdullah <abdullah72308@gmail.com>
2025-09-24 10:27:46 +0600

    un used code removed

--- app/api/expenses/months/routes.js
+++ app/api/expenses/months/routes.js
@@ -1,22 +1,10 @@
 import { NextResponse } from 'next/server'
-import { getAvailableMonths, getExpensesCategoriesByMonth } from '@/lib/expenses'
+import { getAvailableMonths } from '@/lib/expenses'
 
 export async function GET() {
   try {
     const months = await getAvailableMonths()
     return NextResponse.json({ months })
-  } catch (error) {
-    return NextResponse.json({ error: error.message }, { status: 400 })
-  }
-}
-
-export async function GET(request) {
-  try {
-    const { searchParams } = new URL(request.url)
-    const month = searchParams.get('month');
-
-    const categories = await getExpensesCategoriesByMonth(month)
-    return NextResponse.json({ categories })
   } catch (error) {
     return NextResponse.json({ error: error.message }, { status: 400 })
   }
